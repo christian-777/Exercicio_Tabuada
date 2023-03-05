93d7b8099c80056547a47969668b668f9add4fe6
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:01:40 -0300

    Add project files.

---
+++ Exercicio_Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //exercicio tabuada
+
+        int num;
+        int mult;
+
+        Console.WriteLine("digite um numero entre 1 e 9: ");
+        num=int.Parse(Console.ReadLine());
+
+        if(num>0 && num < 10)
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                mult = i * num;
+                Console.WriteLine(num + " * " + i + " = " + mult);
+            }
+        }
+    }
+}
